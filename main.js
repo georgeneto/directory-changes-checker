5943761a3335f9d300fa700b8f8a8b97346e3083
george <george@bornlogic.com>
2022-05-19 19:04:02 -0300

    initial

---
+++ main.js
@@ -0,0 +1,43 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+
+const token = core.getInput("repo-token");
+const directory = core.getInput("directory");
+  
+const octokit = github.getOctokit(token);
+
+
+const run = async () => {
+    console.log(github.context)
+
+    try{
+        let regExp = RegExp(directory)
+
+        console.log(`Raw input: ${directory}`)
+        console.log(`Regex: ${regExp.toString()}`)
+        
+        let response = await octokit.rest.repos.compareCommitsWithBasehead()
+
+        let filteredFiles = response.data.files?.filter(file => {
+            console.log(JSON.parse(file))
+            return regExp.test(file.filename)
+        })
+
+        if(filteredFiles.length == 0){
+            console.log("No matchs found.")
+            console.log(`Raw input: ${directory}`)
+            console.log(`Regex: ${regExp.toString()}`)
+            return false
+        } 
+
+        console.log(`Found a total of ${filteredFiles.length} matches`)
+
+        return true
+
+    }catch(e){
+        console.log("FAIL", e)
+    }
+        
+}
+
+run()
